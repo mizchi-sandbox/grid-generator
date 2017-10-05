01720ccbb98959c9e2ea7448e1af807c76f9224e
mizchi <miz404@gmail.com>
2017-10-05 12:21:11 +0900

    Update uglifyjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 /* eslint-disable */
 // $ yarn add webpack babel-loader
 const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+const ENV = process.env.NODE_ENV || 'development'
 module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3355',
-    'webpack/hot/only-dev-server',
-    './src/index.js'
-  ],
+  entry:
+    ENV !== 'production'
+      ? [
+          'react-hot-loader/patch',
+          'webpack-dev-server/client?http://localhost:3355',
+          'webpack/hot/only-dev-server',
+          './src/index.js'
+        ]
+      : ['react-polyfill', './src/index.js'],
   output: {
     filename: 'bundle.js',
     path: __dirname + '/public',
@@ -29,7 +35,21 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development'
+      )
     })
-  ]
+  ].concat(
+    ENV === 'production'
+      ? [
+          new UglifyJSPlugin({
+            parallel: {
+              cache: true,
+              workers: 2 // for e.g
+            }
+          }),
+          new CompressionPlugin()
+        ]
+      : []
+  )
 }
