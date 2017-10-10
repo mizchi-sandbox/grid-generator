d180d0113dc2830982ca416cec1ea58e076f1dbc
mizchi <miz404@gmail.com>
2017-10-10 22:44:47 +0900

    Support code split

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,24 @@ const webpack = require('webpack')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const CompressionPlugin = require('compression-webpack-plugin')
 const ENV = process.env.NODE_ENV || 'development'
+const pkg = require('./package')
+
+const hmrEntries = [
+  'react-hot-loader/patch',
+  'webpack-dev-server/client?http://localhost:3355',
+  'webpack/hot/only-dev-server'
+]
+
+const deps = Object.keys(pkg.dependencies)
+
 module.exports = {
-  entry:
-    ENV !== 'production'
-      ? [
-          'react-hot-loader/patch',
-          'webpack-dev-server/client?http://localhost:3355',
-          'webpack/hot/only-dev-server',
-          './src/index.js'
-        ]
-      : ['react-polyfill', './src/index.js'],
+  entry: {
+    vendor: (ENV !== 'production' ? hmrEntries : []).concat(deps),
+    app: ['./src/index.js']
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: __dirname + '/public',
     publicPath: '/'
   },
@@ -34,6 +40,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js'
+    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(
         process.env.NODE_ENV || 'development'
